c42d8dae6dfacf3532fc17eca26c432734907640
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2021-02-04 12:33:49 +0200

    remove tested lines

--- src/settings/AppRouter.ts
+++ src/settings/AppRouter.ts
@@ -3,7 +3,6 @@ import express from "express"
 
 export class AppRouter {
     private static instance: express.Router;
-    color: string = "red";
 
     static get getInstance(): express.Router{
         if (!this.instance){
